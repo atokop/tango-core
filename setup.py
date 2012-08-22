5ea1233f154c6c648fdac641dd6ce2e8291aafb3
Ron DuPlain <ron.duplain@willowtreeapps.com>
2012-08-22 12:02:11 -0400

    Require BeautifulSoup v3.2.0.
    v3.2.1 adds some entity encoding which may break the current tango
    scraper tools.

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     zip_safe=False,
     install_requires=[
         'distribute',
-        'BeautifulSoup<4.0',
+        'BeautifulSoup==3.2.0',
         'Flask==0.9',
         'Flask-Script==0.3.2',
         'lxml',
