3b35c380de64d3a53ebafa2beca9084180154204
Ron DuPlain <ron.duplain@willowtreeapps.com>
2012-09-05 16:09:09 -0400

    Add -dev tag explictly.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ sys.path.append('tests/errors')
 
 setup(
     name='Tango',
-    version='0.2.4',
+    version='0.2.4-dev',
     url='http://www.willowtreeapps.com',
     license='BSD',
     maintainer='WillowTree Apps DevOps Team',
